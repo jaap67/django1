25fa3eb548232c866067c96cc23193f4aabf4aa7
Jose <marinhomoraisster@gmail.com>
2020-04-08 23:55:44 -0300

    Apresentando dados do bd no template - aula_31

--- core/views.py
+++ core/views.py
@@ -1,20 +1,24 @@
 from django.shortcuts import render
 
-def index(request):
-    print(dir(request.user))
-    print(f'User: {request.user}')
+from .models import Produto
 
-    if str(request.user) == 'AnonymousUser':
-        teste = 'Usuário não logado.'
-    else:
-        teste = 'Usuário Logado!'
+
+def index(request):
+    produtos = Produto.objects.all()
 
     context = {
         "curso": "Programação Web com Django Framework",
         "outro": "Django é massa!",
-        "logado": teste
+        "produtos": produtos
     }
     return render(request, 'index.html', context)
 
 def contato(request):
     return render(request, 'contato.html')
+
+def produto(request, pk):
+    prod = Produto.objects.get(id=pk)
+    # context = {
+    #     "produto": prod
+    # }
+    return render(request, 'produto.html', {"produto": prod})
